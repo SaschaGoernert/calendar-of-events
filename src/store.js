113c91511025969e48f63687881eb237225c70bd
Sascha Görnert <developer@wirtschaftsinformatik.se>
2020-09-03 23:25:27 +0200

    make calendar editable via local storage
    + add package-lock.json to gitignore

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import { calendarWeekData } from "./data";
 import { uuidv4 } from "./util";
 
+if (!localStorage.getItem("calendar")) {
+  localStorage.setItem("calendar", JSON.stringify(calendarWeekData));
+}
+
 export const store = {
   state: {
-    calendarWeekData,
+    calendarWeekData: JSON.parse(localStorage.getItem("calendar")),
   },
   getActiveDay() {
     return this.state.calendarWeekData.find((day) => day.active);
@@ -21,12 +25,15 @@ export const store = {
       edit: false,
       color: eventColor,
     });
+
+    updateCalendar(this.state.calendarWeekData);
   },
   editEvent(dayId, eventId) {
     this.resetEditMode();
     const day = getDay(this.state, dayId);
     const event = getEvent(day, eventId);
     event.edit = true;
+    updateCalendar(this.state.calendarWeekData);
   },
   resetEditMode() {
     this.state.calendarWeekData.forEach((day) =>
@@ -38,17 +45,21 @@ export const store = {
     const event = getEvent(day, eventId);
     event.title = newEventTitle;
     event.edit = false;
-    console.log("event", event);
+    updateCalendar(this.state.calendarWeekData);
   },
   deleteEvent(dayId, eventId) {
     const day = getDay(this.state, dayId);
     const filteredEvents = day.events.filter((event) => event.id !== eventId);
     day.events = filteredEvents;
+
+    updateCalendar(this.state.calendarWeekData);
   },
   emptyCalendar() {
     this.state.calendarWeekData.forEach((day) => {
       day.events = [];
     });
+
+    updateCalendar(this.state.calendarWeekData);
   },
 };
 
@@ -58,4 +69,8 @@ const getDay = (state, dayId) => {
 
 const getEvent = (day, eventId) => {
   return day.events.find((event) => event.id === eventId);
+};
+
+const updateCalendar = (calendarWeekData) => {
+  localStorage.setItem("calendar", JSON.stringify(calendarWeekData));
 };
